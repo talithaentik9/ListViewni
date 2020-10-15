1d1088c95ba08b61f02c960ce37402e6967c2840
talithaentik9 <dinob3599@gmail.com>
2020-10-15 18:17:30 +0700

    ListViewni

---
+++ app/src/main/java/com/dicoding/picodiploma/mylistview/Hero.kt
@@ -0,0 +1,10 @@
+package com.dicoding.picodiploma.mylistview
+
+import android.accounts.AuthenticatorDescription
+import android.provider.ContactsContract
+
+data class Hero(
+        var photo: Int,
+        var  name: String,
+        var description: String
+)
---
+++ app/src/main/java/com/dicoding/picodiploma/mylistview/HeroAdapter.kt
@@ -0,0 +1,48 @@
+package com.dicoding.picodiploma.mylistview
+
+import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import java.math.MathContext
+import java.net.ContentHandler
+
+class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
+    internal var heroes = arrayListOf<Hero>()
+
+    override fun getCount(): Int = heroes.size
+
+
+    override fun getItem(i: Int): Any = heroes[i]
+
+
+    override fun getItemId(i: Int): Long = i.toLong()
+
+    override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View {
+        var itemView = view
+        if (itemView == null) {
+           itemView = LayoutInflater.from(context).inflate(R.layout.item_hero,viewGroup, false)
+        }
+
+        val viewHolder = ViewHolder(itemView as View)
+
+        val hero = getItem(position) as Hero
+        viewHolder.bind(hero)
+        return itemView
+    }
+    private inner class ViewHolder internal constructor(view: View) {
+        private val txtName: TextView = view.findViewById(R.id.txt_name)
+        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
+        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
+
+        internal fun bind(hero: Hero) {
+            txtName.text = hero.description
+            txtDescription.text = hero.description
+            imgPhoto.setImageResource(hero.photo)
+        }
+    }
+}
